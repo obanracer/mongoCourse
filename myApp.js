ace209041e36ad48b8d6307679f255c1d98eff88
ObanRacer <desimone.brunof@gmail.com>
2021-02-11 14:40:09 -0300

    learning schemas and stuff

--- myApp.js
+++ myApp.js
@@ -1,5 +1,56 @@
 require('dotenv').config();
 
+var mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+  .then(() => {
+    console.log("connected to mongoDB successfully");
+  })
+
+  .catch((err) => {
+    console.log("error :(");
+    console.error(err.stack);
+  });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log("open i guess?");
+
+  const kittySchema = mongoose.Schema({
+    name: String
+  });
+
+  kittySchema.methods.speak = function() {
+    const greeting = this.name ? "Meow name is " + this.name : "I don't have a name";
+    console.log(greeting);
+  };
+
+
+  const Kitten = mongoose.model('Kitten', kittySchema);
+
+  const silence = new Kitten({ name: "Silence" });
+  console.log(silence.name);
+
+  const fluffy = new Kitten({ name: "Fluffy" });
+  fluffy.speak();
+
+  fluffy.save((err, fluffy) => {
+    if (err) return console.error(err);
+    fluffy.speak();
+  });
+
+  silence.save((err, silence) => {
+    if (err) return console.error(err);
+    silence.speak();
+  })
+
+  Kitten.find((err, kittens) => {
+    if (err) return console.error(err);
+    console.log(kittens);
+  });
+});
+
 
 let Person;
 
